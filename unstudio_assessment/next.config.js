089a7f742fa3f5c2fb689bfe94c5a274bdcff618
Shaurya <shauryanayyarviic30@gmail.com>
2024-07-22 21:13:14 +0530

    fix config

--- unstudio_assessment/next.config.js
+++ unstudio_assessment/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {}
+  webpack: (config) => {
+      // You can modify the config here if needed
+      return config; // Ensure the config object is returned
+  }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
