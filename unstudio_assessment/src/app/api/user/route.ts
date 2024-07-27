e10675be5ffb30b3ea11613f34e6077757effca6
Shaurya <77562405+Shaurya61@users.noreply.github.com>
2024-07-27 16:56:59 +0530

    Update route.ts

--- unstudio_assessment/src/app/api/user/route.ts
+++ unstudio_assessment/src/app/api/user/route.ts
@@ -26,13 +26,6 @@ try {
         if (existingUserByEmail) {
             return NextResponse.json({user: null, message: "User with this email already exists"}, {status: 409});
         }
-        const existingUserByUsername = await db.user.findUnique({
-            where: {username: username},
-        });
-
-        if (existingUserByUsername) {
-            return NextResponse.json({user: null, message: "User with this username already exists"}, {status: 409});
-        }
 
     const hashedPassword = await hash(password, 10);
     const newUser = await db.user.create({
